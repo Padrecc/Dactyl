22772219f388d498e8c246420b77a7f75d195c94
Padrecc <padrecc@gmail.com>
2018-10-16 18:28:48 +0300

    Initial commit

---
+++ Dactyl.h
@@ -0,0 +1,13 @@
+#ifndef DACTYL_H
+#define DACTYL_H
+
+#include "quantum.h"
+
+#ifdef KEYBOARD_Dactyl_rev1
+    #include "rev1.h"
+#elif KEYBOARD_Dactyl_rev2
+    #include "rev2.h"
+#endif
+
+
+#endif
---
+++ rev1/keymaps/dvorak/rules.mk
@@ -0,0 +1,5 @@
+TAP_DANCE_ENABLE = no
+RGBLIGHT_ENABLE = no
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
