68d4005ded36bffb32fdccbfd60324d50a7494c2
Alexey <padrecc@gmail.com>
2018-11-23 22:48:17 +0300

    Update config.h

--- rev2/config.h
+++ rev2/config.h
@@ -62,8 +62,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // wiring of each half
 
-#define MATRIX_COL_PINS { B4,E6,D7,D4,D2,D3 }
-#define MATRIX_ROW_PINS { B2,B3,B1,F7,F6,F5 }
+//#define MATRIX_COL_PINS { B4,E6,D7,D4,D2,D3 }
+//#define MATRIX_ROW_PINS { B2,B3,B1,F7,F6,F5 }
 
 
 /* Set 0 if debouncing isn't needed */
