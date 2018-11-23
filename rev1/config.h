d5c5c94f6abb83efeddbbb1b527f6f4f61d8be8b
Alexey <padrecc@gmail.com>
2018-11-23 17:47:54 +0300

    Update config.h

--- rev1/config.h
+++ rev1/config.h
@@ -46,11 +46,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define MATRIX_COL_PINS { B5,B4,E6,D7,C6,D4}
 //#define MATRIX_ROW_PINS { B6,B2, B3, B1,F7, F6,F5 }
 
-#define MATRIX_COL_PINS { B4,E6,D7,D4,D2,D3}
-#define MATRIX_ROW_PINS { B2,B3,B1,F7,F6,F5,F4 }
+//#define MATRIX_COL_PINS { B4,E6,D7,D4,D2,D3}
+//#define MATRIX_ROW_PINS { B2,B3,B1,F7,F6,F5,F4 }
 
-//#define MATRIX_COL_PINS { B4,E6,D7,D4,D2,D3,F4}
-//#define MATRIX_ROW_PINS { B2,B3,B1,F7,F6,F5 }
+#define MATRIX_COL_PINS { B4,E6,D7,D4,D2,D3 }
+#define MATRIX_ROW_PINS { B2,B3,B1,F7,F6,F5 }
 
 
 /* define if matrix has ghost */
