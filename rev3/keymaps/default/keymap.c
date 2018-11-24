7320b02a8b5a30f646c6f73f01f194819d233562
unknown <padrecc@gmail.com>
2018-11-24 15:49:49 +0300

    Fix layer

--- rev3/keymaps/default/keymap.c
+++ rev3/keymaps/default/keymap.c
@@ -1,6 +1,8 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
 
+#include "rev3.h"
+
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
@@ -38,8 +40,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_DELT,         KC_Q,           KC_W,     KC_E,     KC_R,  KC_T,      KC_Y,   KC_U,  KC_I,       KC_O,  KC_P,  KC_BSLS,
           KC_BSPC,         KC_A,           KC_S,     KC_D,     KC_F,  KC_G,      KC_H,   KC_J,  KC_K,       KC_L,  LT(MDIA, KC_SCLN),  GUI_T(KC_QUOT),
           KC_LSFT,  CTL_T(KC_Z),           KC_X,     KC_C,     KC_V,  KC_B,      KC_N,   KC_M,  KC_COMM,  KC_DOT,     CTL_T(KC_SLSH),         KC_RSFT,
-  LT(SYMB,KC_GRV),      KC_QUOT,  LALT(KC_LSFT),  KC_LEFT,  KC_RGHT, KC_NO,    KC_NO,  KC_UP,  KC_DOWN, KC_LBRC,            KC_RBRC,          KC_FN1,
-    ALT_T(KC_APP),      KC_LGUI,        KC_HOME,   KC_SPC,  KC_BSPC,   KC_END,   KC_PGDN, KC_TAB, KC_ENT, KC_PGUP, KC_RALT, CTL_T(KC_ESC)  
+  LT(SYMB,KC_GRV),      KC_QUOT,  LALT(KC_LSFT),  KC_LEFT,  KC_RGHT, KC_NO,     KC_NO,  KC_UP,  KC_DOWN, KC_LBRC,            KC_RBRC,          KC_FN1,
+    ALT_T(KC_APP),      KC_LGUI,        KC_HOME,   KC_SPC,  KC_BSPC,KC_END,   KC_PGDN, KC_TAB, KC_ENT, KC_PGUP, KC_RALT, CTL_T(KC_ESC)  
     ),
 /* Keymap 1: Symbol Layer
  *
--- rev3/keymaps/dvorak/rules.mk
+++
@@ -1,5 +0,0 @@
-TAP_DANCE_ENABLE = no
-RGBLIGHT_ENABLE = no
-ifndef QUANTUM_DIR
-	include ../../../../Makefile
-endif
